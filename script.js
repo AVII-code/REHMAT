d35f0c3b12a971531d8466bb17ebf9657dc8da42
AVII-code <131603390+AVII-code@users.noreply.github.com>
2026-06-30 14:22:30 +0530

    Change secret code and success message in login script

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ const codeInput = document.getElementById('code');
 const submitBtn = document.getElementById('submit-btn');
 const resultDiv = document.getElementById('result');
 
-const secretCode = 'Jannat'; // Replace with your secret code
+const secretCode = 'Khushi'; // Replace with your secret code
 const nextPageUrl = 'main.html'; // Replace with the URL of the new page
 
 form.addEventListener('submit', (e) => {
 	e.preventDefault();
 	const userInput = codeInput.value.trim();
 	if (userInput === secretCode) {
-		resultDiv.innerHTML = 'Baby';
+		resultDiv.innerHTML = 'access granted';
 		window.location.href = nextPageUrl; // Redirect to new page
 	} else {
 		resultDiv.innerHTML = 'U are not the right person ... sorry u cant access';
